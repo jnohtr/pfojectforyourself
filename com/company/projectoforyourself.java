e1b4f2f84cf1d7d48a68efb08d6c96059f9a377f
GBTC440007UR <GBTC440007UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:05:59 -0400

    first commit

---
+++ com/company/projectoforyourself.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class projectoforyourself {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println( "hi, My name is juyeon" );
+        System.out.print( "My age is..." );
+    }
+}
